c8f48b939abeb63b849d1d79e82d6744bdb0ddf5
mgst-d <and8511@ya.ru>
2021-12-16 20:15:54 +0300

    Add settings and migrations

--- db/migrate/20211216162003_create_brands.rb
+++ db/migrate/20211216162003_create_brands.rb
@@ -1,6 +1,10 @@
 class CreateBrands < ActiveRecord::Migration[6.0]
   def change
     create_table :brands do |t|
+      t.string :title
+      t.string :bytitle
+      t.string :img
+      t.string :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_16_162020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.string "title"
+    t.string "bytitle"
+    t.string "img"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "brand_id"
+    t.string "title"
+    t.string "bytitle"
+    t.text "content"
+    t.float "price"
+    t.float "old_price"
+    t.integer "status"
+    t.string "keywords"
+    t.string "description"
+    t.string "img", default: "No image"
+    t.integer "hit", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
