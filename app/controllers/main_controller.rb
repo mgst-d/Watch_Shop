b3fff04803270843271872eb743078c0d175ea80
mgst-d <and8511@ya.ru>
2021-12-17 21:49:04 +0300

    Add meta-tags

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,13 @@
 class MainController < ApplicationController
+
+  before_action :set_page_options
   def index
     @brands = Brand.all.limit(3)
     @products = Product.all.limit(8)
+  end
+  def set_page_options
+    @page_title = "Watches Shop"
+    @page_description = 'Watches'
+    @page_keywords = 'Man Woman Kids'
   end
 end
